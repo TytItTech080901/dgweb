c7a2cd824658836b9760ff46a14b4889e3b60ed3
Fluorine-fish <2256096153@qq.com>
2025-06-29 09:12:26 +0800

    ✨ 新增家长监护页面

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DEBUG_BUTTON_VISIBLE = True  # 控制main.html中调试按钮的显示
 # 语音助手配置
 ENABLE_CHATBOT = True  # 是否启用语音助手
 ENABLE_WELCOME_MESSAGE = False  # 是否启用欢迎消息
-AUTO_START_CHATBOT_LOOP = True  # 是否自动启动语音助手对话循环
+AUTO_START_CHATBOT_LOOP = False  # 是否自动启动语音助手对话循环
 
 # 开放端口配置
 # 使用0.0.0.0表示监听所有可用网络接口，允许任何IP地址连接
